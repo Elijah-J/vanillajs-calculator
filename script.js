a1a5faee5a5dca9f8d7e280db302dd12ed32e81c
Elijah-J <elijukanovich@gmail.com>
2021-02-17 11:58:44 -0800

    implement clear expression button

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function initButtonClickListeners() {
-  let buttons = document.getElementsByTagName("button");
-  [...buttons].forEach((button) => {
-    button.onclick = function () {
-      printToDisplay(button.innerText);
+  let tokenButtons = document.getElementsByClassName("calculator-token");
+  [...tokenButtons].forEach((tokenButton) => {
+    tokenButton.onclick = function () {
+      printToDisplay(tokenButton.innerText);
     };
   });
+
+  let clearButton = document.getElementById("clear-expression");
+  clearButton.onclick = clearDisplay;
 }
 
 function printToDisplay(symbol) {
   let display = document.getElementById("display");
   display.innerText += ` ${symbol}`;
+}
+
+function clearDisplay() {
+  let display = document.getElementById("display");
+  display.innerText = "";
 }
 
 initButtonClickListeners();
