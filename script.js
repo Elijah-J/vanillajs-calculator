41019f322d6a6c43a455cd1e64f6a645d933a904
Eli <30394324+Elijah-J@users.noreply.github.com>
2021-02-17 11:35:04 -0800

    make buttons print their corresponding symbol (1, 2, =, etc.) when pressed

---
+++ script.js
@@ -0,0 +1,15 @@
+function initButtonClickListeners() {
+  let buttons = document.getElementsByTagName("button");
+  [...buttons].forEach((button) => {
+    button.onclick = function () {
+      printToDisplay(button.innerText);
+    };
+  });
+}
+
+function printToDisplay(symbol) {
+  let display = document.getElementById("display");
+  display.innerText = symbol;
+}
+
+initButtonClickListeners();
