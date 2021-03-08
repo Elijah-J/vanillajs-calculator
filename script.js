993e676908413773bb13b08ddb0dd9c09956fecf
Eli <30394324+Elijah-J@users.noreply.github.com>
2021-03-08 15:31:38 -0800

    add expression evaluation funcitonality

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function initButtonClickListeners() {
 
   let clearButton = document.getElementById("clear-expression");
   clearButton.onclick = clearDisplay;
+
+  let solveButton = document.getElementById("solve-expression");
+  solveButton.onclick = function () {
+    solveExpression(document.getElementById("display").innerText);
+  };
 }
 
 function printToDisplay(symbol) {
@@ -15,14 +20,89 @@ function printToDisplay(symbol) {
   if (/^\d+$/.test(symbol)) {
     display.innerText += `${symbol}`;
   } else {
-    display.innerText += `\xa0${symbol}\xa0`;
+    display.innerText += `\xa0${symbol}\xa0 `;
   }
-  console.log(display.innerText);
 }
 
 function clearDisplay() {
   let display = document.getElementById("display");
   display.innerText = "";
+}
+
+function printResult(result) {
+  let display = document.getElementById("display");
+  display.innerText = result;
+}
+
+function solveExpression(expression) {
+  let tokenizedExpression = tokenize(expression);
+  let postfixExpression = convertFromInfixToPostfix(tokenizedExpression);
+  let evaluatedExpression = evaluatePostfixExpression(postfixExpression);
+
+  printResult(evaluatedExpression);
+}
+
+function tokenize(expression) {
+  return expression.split(/\s/);
+}
+
+function convertFromInfixToPostfix(tokenizedExpression) {
+  const precedence = {
+    "*": 2,
+    "/": 2,
+    "+": 1,
+    "-": 1,
+  };
+
+  let operatorStack = [];
+  let postfixList = [];
+
+  for (let i = 0; i < tokenizedExpression.length; i++) {
+    token = tokenizedExpression[i];
+    if (token.match(/\d/)) {
+      postfixList.push(token);
+    } else {
+      while (
+        operatorStack.length > 0 &&
+        precedence[operatorStack[operatorStack.length - 1]] >= precedence[token]
+      ) {
+        postfixList.push(operatorStack.pop());
+      }
+
+      operatorStack.push(token);
+    }
+  }
+
+  while (operatorStack.length > 0) {
+    postfixList.push(operatorStack.pop());
+  }
+
+  return postfixList;
+}
+
+function evaluatePostfixExpression(postfixExpression) {
+  let operandStack = [];
+  for (let i = 0; i < postfixExpression.length; i++) {
+    let token = postfixExpression[i];
+
+    if (token.match(/\d/)) {
+      operandStack.push(token);
+    } else {
+      let operandTwo = operandStack.pop();
+      let operandOne = operandStack.pop();
+      let result = doMath(operandOne, operandTwo, postfixExpression[i]);
+      operandStack.push(result);
+    }
+  }
+
+  return operandStack.pop();
+}
+
+function doMath(operandOne, operandTwo, operator) {
+  if (operator === "+") return operandOne + operandTwo;
+  else if (operator === "-") return operandOne - operandTwo;
+  else if (operator === "*") return operandOne * operandTwo;
+  else if (operator === "/") return operandOne / operandTwo;
 }
 
 initButtonClickListeners();
